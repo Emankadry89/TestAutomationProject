c71d95f271d1b4c9843a0589dbe9ec247f7ae9fc
eman-Kadry <113099445+eman-Kadry@users.noreply.github.com>
2025-01-05 01:24:59 +0200

    Solve warning

--- testAutomationProject/src/test/java/testPackage/UploadFileTests.java
+++ testAutomationProject/src/test/java/testPackage/UploadFileTests.java
@@ -7,7 +7,6 @@ import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-import pagePackage.CheckBoxesPage;
 import pagePackage.FileUploadedPage;
 import pagePackage.UploadFilePage;
 
